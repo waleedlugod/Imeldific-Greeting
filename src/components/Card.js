7ec79127c7e1f12d0d3642259757bd80a3ef4eb3
Waleed Lugod <lugodwaleed@gmail.com>
2022-10-09 23:17:09 +0800

    added arrow svg

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { ReactComponent as DownArrow } from "../images/down-arrow.svg";
+import { ReactComponent as UpArrow } from "../images/up-arrow.svg";
 
 function Card({ q, a }) {
 	const [show, setShow] = useState(false);
@@ -11,6 +13,7 @@ function Card({ q, a }) {
 		<>
 			<button onClick={toggleAnswer}>
 				<h2>{q}</h2>
+				{show ? <UpArrow /> : <DownArrow />}
 			</button>
 			{show &&
 				a.map((text) => {
