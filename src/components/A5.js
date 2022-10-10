ad3121214cc94432a442e923d3b972aa2a22bc63
Waleed Lugod <lugodwaleed@gmail.com>
2022-10-10 22:17:44 +0800

    added images and refactored heading img css

--- src/components/A5.js
+++ src/components/A5.js
@@ -1,12 +1,13 @@
 import React from "react";
+import tweet1 from "../images/tweet1.png";
+import tweet2 from "../images/tweet2.png";
+import tweet3 from "../images/tweet3.png";
+import lg from "../images/lauren-greenfield.png";
+import dy from "../images/darryl-yap.png";
 
 function A5() {
 	return (
 		<>
-			<p>
-				What is the effect of the message being conveyed in this manner to the
-				receiver? Who benefits
-			</p>
 			<p>
 				The effect of the billboard greeting differs depending on the receiver’s
 				political stance. Those who sided with Leni’s campaign found the
@@ -15,6 +16,11 @@ function A5() {
 				saw this as an opportunity to ridicule the former First Lady. Some
 				tweets even mentioned her graft cases in their “greetings.”
 			</p>
+			<div className="image-container">
+				<img src={tweet1} alt="" />
+				<img src={tweet2} alt="" />
+				<img src={tweet3} alt="" />
+			</div>
 			<p>
 				Moreover, a prominent individual also reacted to the electronic
 				billboard. She is Lauren Greenfield, the writer and director of the
@@ -23,6 +29,7 @@ function A5() {
 				documentary she produced. She claimed that the image was stolen from her
 				and that the billboard violated copyright laws.
 			</p>
+			<img src={lg} alt="" />
 			<p>
 				On the other hand, Marcos’ supporters did not, in any way, appreciate
 				the billboard greeting. Darryl Yap, a known staunch supporter of the
@@ -32,6 +39,7 @@ function A5() {
 				He tried to solidify his theory by pointing out that the image used was
 				from “The Kingmaker,” an anti-Marcos documentary.
 			</p>
+			<img src={dy} alt="" />
 			<p>
 				It’s hard to tell who really benefited from this billboard greeting. On
 				social media, we can clearly see that this did not favor the Marcoses.
