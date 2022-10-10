e478614d165a0bd39b96fd06cf14786b820508a2
Waleed Lugod <lugodwaleed@gmail.com>
2022-10-10 21:54:18 +0800

    added styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,13 @@ import React from "react";
 
 function Footer() {
 	return (
-		<p className="footer">
-			Andre Sulit Canoy, Andrew Emmanuel Robles, Joseph Sagusay, Jv Warain,
-			Waleed Lugod
-		</p>
+		<div className="footer">
+			<p>Andre Sulit Canoy</p>
+			<p>Andrew Emmanuel Robles</p>
+			<p>Joseph Sagusay</p>
+			<p>Jv Warain</p>
+			<p>Waleed Lugod</p>
+		</div>
 	);
 }
 
