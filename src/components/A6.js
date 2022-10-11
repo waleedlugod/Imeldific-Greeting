97f5f6d6784ef00c1ce3519591d80cea57a0ee42
Waleed Lugod <lugodwaleed@gmail.com>
2022-10-11 16:54:00 +0800

    i dont even know anymore

--- src/components/A6.js
+++ src/components/A6.js
@@ -3,7 +3,20 @@ import React from "react";
 function A6() {
 	return (
 		<div className="answer-container">
-			<p>not answered</p>
+			<p>
+				Aside from the politically active Filipinos, another perspective would
+				be from individuals who are politically inactive. In this case, they may
+				view the message simply as an extravagant greeting for an important
+				person. This is because the main purpose of the message is to greet
+				Imelda Marcos. Another perspective would be from the people who do not
+				use social media and have seen the billboard themselves in real life. In
+				this case, they may actually not pay attention to every detail of the
+				message. They may not notice the spelling error or not be aware of the
+				fact that the image used to depict Imelda Marcos is from the “The
+				Kingmaker”. Since they do not delve deeper into the details, a Marcos
+				supporter who sees this billboard may actually approve and appreciate
+				the billboard greeting.
+			</p>
 		</div>
 	);
 }
