58fcf01ba573793cad54f67af80167e859356d9e
Waleed Lugod <lugodwaleed@gmail.com>
2022-10-11 11:55:21 +0800

    added answer for 2

--- src/components/A2.js
+++ src/components/A2.js
@@ -3,7 +3,13 @@ import React from "react";
 function A2() {
 	return (
 		<div className="answer-container">
-			<p>not answered</p>
+			<p>
+				The purpose of the message is to greet Imelda Marcos on her 93rd
+				birthday as mentioned earlier. It also shows that the Marcos era is back
+				through calling Imelda Marcos the “First Lady” in the billboard.
+				Moreover, the Marcos family is showing their return to power through
+				this giant billboard of Imelda Marcos.
+			</p>
 		</div>
 	);
 }
