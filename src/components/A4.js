4a8bfe88988ff0e2547697d9118e29aa8c95aad0
Waleed Lugod <lugodwaleed@gmail.com>
2022-10-11 12:15:01 +0800

    changed answer of 4

--- src/components/A4.js
+++ src/components/A4.js
@@ -4,8 +4,10 @@ function A4() {
 	return (
 		<div className="answer-container">
 			<p>
-				The target audience is the politically active filipinos. More
-				specifically, supporters of Bongbong Marcos Jr.
+				The target audience is the politically active Filipinos. More
+				specifically, supporters of Imelda Marcos, and the Marcos family.
+				However, because of the ironic context surrounding the image, opposing
+				political parties took greater interest as an opportunity for mockery.
 			</p>
 		</div>
 	);
