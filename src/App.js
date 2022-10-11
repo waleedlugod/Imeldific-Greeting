219c1216826af25718b1a608f784a8bb8197cd84
Waleed Lugod <lugodwaleed@gmail.com>
2022-10-11 11:52:53 +0800

    refactored easter egg

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import EasterEgg from "./components/EasterEgg";
 import photo from "./images/photo.jpg";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 			<img className="primary-photo" src={photo} alt="" />
 			<Main />
 			<Footer />
+			<EasterEgg />
 		</div>
 	);
 }
---
+++ src/components/EasterEgg.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function EasterEgg() {
+	return (
+		<div className="easter-egg">
+			hi chesca if ur stalking my code c: here is the repo
+			https://github.com/waleedlugod/Imeldific-Greeting
+		</div>
+	);
+}
+
+export default EasterEgg;
